3ad2fa8c3cf65ef598c899acc630073409bab2eb
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-29 21:28:55 +0200

    megkuzd() metodus megirasa

--- src/hu/home/harcosoop/Harcos.java
+++ src/hu/home/harcosoop/Harcos.java
@@ -12,12 +12,28 @@ public class Harcos {
 
 
     //constructors
-    public void Harcos(String nev, int statuszSablon){
-
-
+    public Harcos(String nev, int statuszSablon){
+        this.nev = nev;
+        this.szint = 1;
+        this.tapasztalat = 0;
+        switch (statuszSablon){
+            case 1:
+                this.alapEletero = 15;
+                this.alapSebzes = 3;
+                break;
+            case 2:
+                this.alapEletero = 12;
+                this.alapSebzes = 4;
+                break;
+            case 3:
+                this.alapEletero = 8;
+                this.alapSebzes = 5;
+                break;
+        }
+        this.eletero = getMaxEletero();
     }
 
-    //getters and setters
+    //getters
     public String getNev() {
         return nev;
     }
@@ -42,6 +58,7 @@ public class Harcos {
         return alapSebzes;
     }
 
+    //setters
     public void setNev(String nev) {
         this.nev = nev;
     }
@@ -60,19 +77,49 @@ public class Harcos {
 
     //methods
     public int getSebzes(){
-
+        return this.alapSebzes + this.szint;
     }
 
     public int getSzintLepeshez(){
-
+        return 10 + this.szint*5;
     }
 
     public int getMaxEletero(){
-
+        return this.alapEletero + szint*3;
     }
 
     public void megkuzd(Harcos masikHarcos){
-
+        if (this == masikHarcos){
+            System.out.println("Hiba! A megadott harcos nem megfelelő");
+            return;
+        }
+        if (this.getEletero() == 0 ||masikHarcos.getEletero() == 0){
+            System.out.println("Valamelyik harcos már elesett a harcban");
+            return;
+        }
+        boolean kihivoTamad = true;
+        boolean kihivoGyozott = true;
+        while (this.getEletero() > 0 && masikHarcos.getEletero() > 0){
+            if (kihivoTamad){
+                masikHarcos.setEletero(masikHarcos.getEletero() - this.getSebzes());
+                kihivoTamad = false;
+            }else if (!kihivoTamad){
+                this.setEletero(this.getEletero() - masikHarcos.getSebzes());
+                kihivoTamad = true;
+            }
+            if (this.getEletero() > 0){
+                this.setTapasztalat(this.getTapasztalat() + 5);
+            }
+            if (masikHarcos.getEletero() > 0){
+                masikHarcos.setTapasztalat(masikHarcos.getTapasztalat() + 5);
+            }
+        }
+        if (masikHarcos.getEletero() > 0){
+            kihivoGyozott = false;
+            masikHarcos.setTapasztalat(masikHarcos.getTapasztalat() + 10);
+        }else {
+            this.setTapasztalat(this.getTapasztalat() + 10);
+        }
     }
 
     public void gyogyul(){
@@ -83,6 +130,8 @@ public class Harcos {
 
     @Override
     public String toString() {
-        return "Harcos{}";
+        return this.nev + " - LVL: " + this.szint
+                + " - EXP: " + this.tapasztalat + "/" + this.getSzintLepeshez()
+                + " - HP: " + this.eletero + "/" + this.getMaxEletero() + " - DMG: " + this.getSebzes();
     }
 }
--- src/hu/home/harcosoop/Main.java
+++ src/hu/home/harcosoop/Main.java
@@ -3,5 +3,15 @@ package hu.home.harcosoop;
 public class Main {
     public static void main(String[] args) {
 
+        Harcos jani = new Harcos("jani", 2);
+        Harcos telepiJozsi = new Harcos("a telepi józsi", 1);
+
+        System.out.println(jani);
+        System.out.println(telepiJozsi);
+
+        jani.megkuzd(telepiJozsi);
+
+        System.out.println(jani);
+        System.out.println(telepiJozsi);
     }
 }
